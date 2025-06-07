913cc4ac058a63294d8c1d94001882466f700239
Pat Heard <patrick.heard@cds-snc.ca>
2025-06-07 23:26:35 +0000

    feat: add SES and user for email

--- terraform/aws/route53.tf
+++ terraform/aws/route53.tf
@@ -14,3 +14,44 @@ resource "aws_route53_record" "n8n_A" {
     evaluate_target_health = false
   }
 }
+
+# SES domain validation
+resource "aws_route53_record" "n8n_ses_verification_TXT" {
+  zone_id = aws_route53_zone.n8n.zone_id
+  name    = "_amazonses.${aws_ses_domain_identity.n8n.id}"
+  type    = "TXT"
+  ttl     = "600"
+  records = [aws_ses_domain_identity.n8n.verification_token]
+}
+
+# Email sending
+resource "aws_route53_record" "n8n_spf_TXT" {
+  zone_id = aws_route53_zone.n8n.zone_id
+  name    = var.domain
+  type    = "TXT"
+  ttl     = "300"
+  records = [
+    "v=spf1 include:amazonses.com -all"
+  ]
+}
+
+resource "aws_route53_record" "n8n_dkim_CNAME" {
+  count   = 3
+  zone_id = aws_route53_zone.n8n.zone_id
+  name    = "${element(aws_ses_domain_dkim.n8n.dkim_tokens, count.index)}._domainkey.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [
+    "${element(aws_ses_domain_dkim.n8n.dkim_tokens, count.index)}.dkim.amazonses.com",
+  ]
+}
+
+resource "aws_route53_record" "n8n_dmarc_TXT" {
+  zone_id = aws_route53_zone.n8n.zone_id
+  name    = "_dmarc.${var.domain}"
+  type    = "TXT"
+  ttl     = "300"
+  records = [
+    "v=DMARC1; p=reject; sp=reject; pct=100; rua=mailto:CDS.SECURITY-SECURITE.SNC@servicecanada.gc.ca"
+  ]
+}
---
+++ terraform/aws/ses.tf
@@ -0,0 +1,58 @@
+#
+# Send email using a SES SMTP server
+#
+resource "aws_ses_domain_identity" "n8n" {
+  domain = var.domain
+}
+
+resource "aws_ses_domain_dkim" "n8n" {
+  domain = aws_ses_domain_identity.n8n.domain
+}
+
+resource "aws_ses_domain_identity_verification" "n8n" {
+  domain     = aws_ses_domain_identity.n8n.id
+  depends_on = [aws_route53_record.n8n_ses_verification_TXT]
+}
+
+resource "aws_iam_user" "n8n_send_email" {
+  # checkov:skip=CKV_AWS_273: SES IAM user is required to confirgure SMTP credentials
+  name = "n8n_send_email"
+}
+
+resource "aws_iam_group" "n8n_send_email" {
+  name = "n8n_send_email"
+}
+
+resource "aws_iam_group_membership" "n8n_send_email" {
+  name  = aws_iam_user.n8n_send_email.name
+  group = aws_iam_group.n8n_send_email.name
+  users = [
+    aws_iam_user.n8n_send_email.name
+  ]
+}
+
+resource "aws_iam_group_policy_attachment" "n8n_send_email" {
+  group      = aws_iam_user.n8n_send_email.name
+  policy_arn = aws_iam_policy.n8n_send_email.arn
+}
+
+data "aws_iam_policy_document" "n8n_send_email" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ses:SendRawEmail"
+    ]
+    resources = [
+      aws_ses_domain_identity.n8n.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "n8n_send_email" {
+  name   = "n8n_send_email"
+  policy = data.aws_iam_policy_document.n8n_send_email.json
+}
+
+resource "aws_iam_access_key" "n8n_send_email" {
+  user = aws_iam_user.n8n_send_email.name
+}
